3c03c0ca27cf76bab1a70c974c7b0fb784bc9919
Jonathan Ng <jon7ng@gmail.com>
2014-12-01 12:25:13 -0800

    added css file, gave each section a color, added users and friends

--- app.js
+++ app.js
@@ -6,13 +6,43 @@ angular.module('app', [])
 	},
 	$scope.lists = [
 		{
+			id: 1,
 			name: 'Climbing',
 			items: ['harness', 'climbing shoes']
 		},
 		{
+			id: 2,
 			name: "Camping",
 			items: ['sleeping bag', 'tarp']
+		},
+		{
+			id: 3,
+			name: "Ice Climbing",
+			items: ['crampons', 'helmet']
 		}
+	],
+	$scope.users = [
+	{
+		id: 1,
+		name: 'Jonathan Ng',
+		listIds: [1, 2],
+		personalizedLists: [],
+		friends: ['John Doe']
+	},
+	{
+		id: 2,
+		name: 'John Doe',
+		listIds: [2, 3],
+		personalizedLists: [],
+		friends: ['Jonathan Ng', 'Jane Doe']
+	},
+	{
+		id: 3,
+		name: 'Jane Doe',
+		listIds: [3],
+		personalizedLists: [],
+		friends: ['John Doe']
+	}
 	]
 });
 
